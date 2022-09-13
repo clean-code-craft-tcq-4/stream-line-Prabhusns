5cba51c7bb6f522d4040fdcc6b7ecdd04eb885c7
Prabhu Somasundaram (SX/EHC31) <Somasundaram.Prabhu@in.bosch.com>
2022-09-13 17:23:59 +0530

    Updated code

---
+++ src/BMSUtil.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class BMSUtil {
+
+	public static List<Integer> createSeq(int startVal, int endVal, int step) {
+		return IntStream.iterate(startVal, d -> d <= endVal, d -> d + step).boxed().collect(Collectors.toList());
+	}
+
+	public static List<Integer> createTempSeq(int startVal, int endVal, int step) {
+		return createSeq(startVal, endVal, step);
+	}
+
+	public static List<Integer> createCRSeq(int startVal, int endVal, int step) {
+		return createSeq(startVal, endVal, step);
+	}
+
+	public static void printSeq(String msg, final List<Integer> seqList) {
+		System.out.println(msg + " - " + seqList);
+	}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Main {
+
+	public static void main(final String[] args) {
+
+		List<Integer> tempValues = BMSUtil.createTempSeq(20, 100, 5);
+		BMSUtil.printSeq("Temperature Sequence", tempValues);
+
+		List<Integer> cRValues = BMSUtil.createCRSeq(2, 50, 2);
+		BMSUtil.printSeq("Charge Rate Sequence", cRValues);
+
+	}
+}
