f4aa32839e175f8e91dabd0eb0a421880ad836d9
AbhiPalankar <105204004+AbhiPalankar@users.noreply.github.com>
2022-10-17 19:27:17 +0530

    Add files via upload
    Receiver code files

---
+++ Receiver/BMS_SensorData_Receiver.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "BMS_SensorData_Receiver.h"
+
+int ReadBatteryTemp[5];
+int ReadBatterySoc[5];
+int arrNumbers[5] = {0};
+
+int pos = 0;
+int BatteryTempAvg = 0;
+int BatterySocAvg = 0;
+long sum = 0;
+
+int len = sizeof(arrNumbers) / sizeof(int);
+int count = sizeof(ReadBatteryTemp) / sizeof(int);
+
+void ReadBatteryParameter(int min,int max,int *ReadBatteryTemp,int *ReadBatterySoc)
+{
+    printf("**************Read Sensor Data**************\n");
+    if(ReadBatteryTemp!=NULL)
+    {
+        for(int i = min;i<max;i++)
+        {
+        printf("Read Battery Temprature Sensor data:%d,Read Battery SOC Sensor data:%d\n",ReadBatteryTemp[i],ReadBatterySoc[i]);
+        }
+    }
+}
+
+int Simple_Moving_Avg(int *arrNumbers, long *sum, int pos, int len, int nextNum)
+{
+  *sum = *sum - arrNumbers[pos] + nextNum;
+   arrNumbers[pos] = nextNum;
+  return *sum / len;
+}
+
+void Calculate_Avg(int *ReadBatteryTemp,int *ReadBatterySoc)
+{
+    for(int i = 0; i < count; i++)
+    {
+    BatteryTempAvg = Simple_Moving_Avg(arrNumbers, &sum, pos, len, ReadBatteryTemp[i]);
+    BatterySocAvg = Simple_Moving_Avg(arrNumbers, &sum, pos, len, ReadBatterySoc[i]);
+    printf("The Battery Temprature Average is %d,The Battery SOC Average %d\n", BatteryTempAvg,BatterySocAvg);
+    pos++;
+    if (pos >= len)
+        {
+            pos = 0;
+        }
+    }
+}
---
+++ Receiver/BMS_SensorData_Receiver.h
@@ -0,0 +1,4 @@
+void ReadBatteryParameter(int min,int max,int *ReadBatteryTemp,int *ReadBatterySoc);
+
+int Simple_Moving_Avg(int *arrNumbers, long *sum, int pos, int len, int nextNum);
+void Calculate_Avg(int *ReadBatteryTemp,int *ReadBatterySoc);
