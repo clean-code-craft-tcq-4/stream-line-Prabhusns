a25dc556242f337c75531a1908c2bacb0c342668
AbhiPalankar <105204004+AbhiPalankar@users.noreply.github.com>
2022-10-26 19:35:17 +0530

    Update BMS_SensorData_Receiver.h

--- Receiver/BMS_SensorData_Receiver.h
+++ Receiver/BMS_SensorData_Receiver.h
@@ -1,4 +1,4 @@
 void ReadBatteryParameter(int min,int max,int *ReadBatteryTemp,int *ReadBatterySoc);
-
+void ReadFromConsoleInput(float* temperature, float* SOC, float* ChargeRate);
 int Simple_Moving_Avg(int *arrNumbers, long *sum, int pos, int len, int nextNum);
 void Calculate_Avg(int *ReadBatteryTemp,int *ReadBatterySoc);
