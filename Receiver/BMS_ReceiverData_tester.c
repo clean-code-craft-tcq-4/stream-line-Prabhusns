513b0fe1b7e30fab87e0e822177fe8d449804eab
AbhiPalankar <105204004+AbhiPalankar@users.noreply.github.com>
2022-10-26 20:30:09 +0530

    Update BMS_ReceiverData_tester.c

--- Receiver/BMS_ReceiverData_tester.c
+++ Receiver/BMS_ReceiverData_tester.c
@@ -6,9 +6,12 @@ int sampleValue[5] = {1,2,3,4,5};
 int sampleValue1[5] = {1,2,3,4,5};
 
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-  printf("argc =%d argv=%s \n\r", argc, argv);
+  int i;
+  for (i = 0; i < argc; i++){
+    printf("arg%i = %s\n", i, argv[i]);
+  }
   ReadBatteryParameter(0,5,sampleValue,sampleValue1);
   Calculate_Avg(sampleValue,sampleValue1);
 }
