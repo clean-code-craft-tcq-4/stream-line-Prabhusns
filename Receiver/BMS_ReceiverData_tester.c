c6095677cdf28ab15aa89d35a4ff7fad37df3aa1
AbhiPalankar <105204004+AbhiPalankar@users.noreply.github.com>
2022-10-26 19:25:01 +0530

    Update BMS_ReceiverData_tester.c

--- Receiver/BMS_ReceiverData_tester.c
+++ Receiver/BMS_ReceiverData_tester.c
@@ -1,12 +1,18 @@
 #include <assert.h>
 #include "BMS_SensorData_Receiver.h"
 
-int sampleValue[5] = {1,2,3,4,5};
-int sampleValue1[5] = {1,2,3,4,5};
+//int sampleValue[5] = {1,2,3,4,5};
+//int sampleValue1[5] = {1,2,3,4,5};
 
+void ReadParameters(float* temperature, float* SOC, float* ChargeRate)
+{
+  char dummy;
+  scanf("%f%c%f%c%f", temperature, &dummy, SOC, &dummy, ChargeRate);
+}
 
 int main(void) {
-
-	ReadBatteryParameter(0,5,sampleValue,sampleValue1);
-    Calculate_Avg(sampleValue,sampleValue1);
+ReadParameters(temperature, SOC, ChargeRate);
+printf("temperature = %f, SOC = %f, ChargeRate = %f\n", temperature, SOC, ChargeRate);	
+//	ReadBatteryParameter(0,5,sampleValue,sampleValue1);
+//    Calculate_Avg(sampleValue,sampleValue1);
 }
